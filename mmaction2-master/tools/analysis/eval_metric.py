05d4648ded01c25760e6bc2d0309359b588b0457
root <zhangzhilin@whu.edu.cn>
2025-03-16 14:53:50 +0800

    文件名

---
+++ mmaction2-master/tools/analysis/eval_metric.py
@@ -0,0 +1,67 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import argparse
+
+import mmcv
+from mmcv import Config, DictAction
+
+from mmaction.datasets import build_dataset
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Evaluate metric of the '
+                                     'results saved in pkl/yaml/json format')
+    parser.add_argument('--config',default="/root/autodl-tmp/mmaction2-master/configs/detection/ava/my_slowfast_kinetics_pretrained_r50_4x16x1_20e_ava_rgb_custom_classes.py", help='Config of the model')
+    parser.add_argument('--results',default="/root/autodl-tmp/mmaction2-master/work_dirs/ava/slowfast_kinetics_pretrained_r50_4x16x1_20e_ava_rgb_custom/20241214_183126.log.json", help='Results in pkl/yaml/json format')
+    parser.add_argument(
+        '--eval',
+        type=str,
+        default="mean_class_accuracy",
+        nargs='+',
+        help='evaluation metrics, which depends on the dataset, e.g.,'
+        ' "top_k_accuracy", "mean_class_accuracy" for video dataset')
+    parser.add_argument(
+        '--cfg-options',
+        nargs='+',
+        action=DictAction,
+        default={},
+        help='override some settings in the used config, the key-value pair '
+        'in xxx=yyy format will be merged into config file. For example, '
+        "'--cfg-options model.backbone.depth=18 model.backbone.with_cp=True'")
+    parser.add_argument(
+        '--eval-options',
+        nargs='+',
+        action=DictAction,
+        help='custom options for evaluation, the key-value pair in xxx=yyy '
+        'format will be kwargs for dataset.evaluate() function')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+
+    cfg = Config.fromfile(args.config)
+
+    assert args.eval is not None
+
+    if args.cfg_options is not None:
+        cfg.merge_from_dict(args.cfg_options)
+    cfg.data.test.test_mode = True
+
+    dataset = build_dataset(cfg.data.test)
+    outputs = mmcv.load(args.results)
+
+    kwargs = {} if args.eval_options is None else args.eval_options
+    eval_kwargs = cfg.get('evaluation', {}).copy()
+    # hard-code way to remove EvalHook args
+    for key in [
+            'interval', 'tmpdir', 'start', 'gpu_collect', 'save_best', 'rule',
+            'by_epoch'
+    ]:
+        eval_kwargs.pop(key, None)
+    eval_kwargs.update(dict(metrics=args.eval, **kwargs))
+    print(dataset.evaluate(outputs, **eval_kwargs))
+
+
+if __name__ == '__main__':
+    main()
